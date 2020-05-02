19c4c4b1003800e39aacada8c39315e9164180a6
carlos7ags <carlos7ags@gmail.com>
2020-05-01 22:23:31 -0500

    Correct file name

--- walmart_cornershop/walmart_cornershop/spiders/walmart_ca.py
+++ walmart_cornershop/walmart_cornershop/spiders/walmart_ca.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from ..items import ProductBranchItem, ProductItem
+
 
 class WalmarCaSpider(scrapy.Spider):
-    name = 'walmar-ca'
+    name = 'walmart-ca'
     allowed_domains = ['walmart.ca']
-    start_urls = ['http://walmart.ca/']
+    start_urls = ['https://www.walmart.ca/en/grocery/N-117']
 
     def parse(self, response):
         pass
