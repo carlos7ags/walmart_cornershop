b02fb4e02bdbc1fc0927361c5508010738533a3b
carlos7ags <carlos7ags@gmail.com>
2020-05-01 22:05:54 -0500

    Create Item models for Product and BranchProduct

--- walmart_cornershop/walmart_cornershop/items.py
+++ walmart_cornershop/walmart_cornershop/items.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
+class ProductItem(scrapy.Item):
+    store = scrapy.Field()
+    sku = scrapy.Field()
+    barcodes = scrapy.Field()
+    brand = scrapy.Field()
+    name = scrapy.Field()
+    description = scrapy.Field()
+    package = scrapy.Field()
+    image = scrapy.Field()
+    category = scrapy.Field()
+    url = scrapy.Field()
 
-class WalmartCornershopItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class BranchProductItem(scrapy.Item):
+    product = scrapy.Field()
+    branch = scrapy.Field()
+    stock = scrapy.Field()
+    price = scrapy.Field()
