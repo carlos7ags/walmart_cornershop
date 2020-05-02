6bdde7a20e253a88f9cf7f79431eb84c2dd92536
carlos7ags <carlos7ags@gmail.com>
2020-05-01 22:24:28 -0500

    Def parse function to get all urls from sites

--- walmart_cornershop/walmart_cornershop/spiders/walmart_ca.py
+++ walmart_cornershop/walmart_cornershop/spiders/walmart_ca.py
@@ -10,4 +10,17 @@ class WalmarCaSpider(scrapy.Spider):
     start_urls = ['https://www.walmart.ca/en/grocery/N-117']
 
     def parse(self, response):
+        # Get all urls in every page
+        urls = response.xpath('*//a/@href').getall()
+        for url in urls:
+            url = response.urljoin(url)
+            yield scrapy.Request(url=url, callback=self.parse_product)
+
+        # Get next page if list of products page
+        next_page_url = response.xpath('//*[@id="loadmore"]/@href').get()
+        if next_page_url:
+            next_page_url = response.urljoin(next_page_url)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
+
+    def parse_product(self, response):
         pass
