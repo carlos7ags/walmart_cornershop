743ad3e60e4ae45384bc994359669b204f1f19cd
carlos7ags <carlos7ags@gmail.com>
2020-05-01 23:17:56 -0500

    Change from parse method to rules

--- walmart_cornershop/walmart_cornershop/spiders/walmart_ca.py
+++ walmart_cornershop/walmart_cornershop/spiders/walmart_ca.py
@@ -3,27 +3,19 @@
 import scrapy
 import json
 from ..items import BranchProductItem, ProductItem
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
-
-class WalmartCaSpider(scrapy.Spider):
+class WalmartCaSpider(CrawlSpider):
     name = 'walmart-ca'
     allowed_domains = ['walmart.ca']
     start_urls = ['https://www.walmart.ca/en/grocery/N-117']
+    base_url = 'https://www.walmart.ca/'
 
     item_count = 0
 
-    def parse(self, response):
-        # Get all urls in every page
-        urls = response.xpath('*//a/@href').getall()
-        for url in urls:
-            url = response.urljoin(url)
-            yield scrapy.Request(url=url, callback=self.parse_product)
-
-        # Get next page if list of products page
-        next_page_url = response.xpath('//*[@id="loadmore"]/@href').get()
-        if next_page_url:
-            next_page_url = response.urljoin(next_page_url)
-            yield scrapy.Request(url=next_page_url, callback=self.parse)
+    rules = [Rule(LinkExtractor(allow='en/ip/'),
+                  callback='parse_product', follow=True)]
 
 
     def parse_product(self, response):
